f3af16336576ac04b5a891c8ac7580d8e15f9e03
Avocado-web <igorahparov@gmail.com>
2021-08-20 20:44:32 +0300

    create: js

--- js/main.js
+++ js/main.js
@@ -2,28 +2,34 @@ $(document).ready(function () {
   var currentFloor = 2;
   var counterUp = $(".main-info__counter-up")
   var counterDown = $(".main-info__counter-down")
-  $('.main-image__home path').on("mouseover", function(){
+  var floorPath = $(".main-image__home path");
+
+
+  floorPath.on("mouseover", function(){
+    floorPath.removeClass("current-floor");
     currentFloor = $(this).attr("data-floor");
     $(".main-info__counter").text(currentFloor);
   });
 
+
   counterUp.on("click", function(){
-   if (currentFloor < 18) {
-    currentFloor++
-    usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
-    $(".main-info__counter").text(usCurrentFloor);
-    $(".main-image__home path").removeClass("current-floor");
-    $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
-   }
+    if (currentFloor < 18) {
+      currentFloor++;
+      usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+      $(".main-info__counter").text(usCurrentFloor);
+      floorPath.removeClass("current-floor");
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
+    }
   });
   
+  
   counterDown.on("click", function(){
-   if (currentFloor > 2) {
-    currentFloor--
-    usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
-    $(".main-info__counter").text(usCurrentFloor);
-    $(".main-image__home path").removeClass("current-floor");
-    $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
-   }
+    if (currentFloor > 2) {
+      currentFloor--;
+      usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+      $(".main-info__counter").text(usCurrentFloor);
+      floorPath.removeClass("current-floor");
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
+    }
   });
 });
